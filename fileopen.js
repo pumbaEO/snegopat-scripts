8bad6e1868bfc1197dc52238ea8f81ed8ecf8941
artbear <artbear>
2011-10-04 13:31:37 +0000

    Добавил перехват сохранения файлов *.epf, *.erf и их немедленное открытие в Конфигураторе - http://snegopat.ru/forum/viewtopic.php?f=3&t=33

--- fileopen.js
+++ fileopen.js
@@ -6,6 +6,92 @@ $addin stdcommands
 
 global.connectGlobals(SelfScript)
 
+/* Скрипт для перехвата сохранения файлов для Снегопата и немедленного открытия файлов
+ * Автор		: Артур Аюханов aka artbear artbear@gmail.com
+ * Дата создания: 04.10.2011
+ * Описание		: http://snegopat.ru/forum/viewtopic.php?f=3&t=33
+    Скрипт "Немедленное открытие файлов, сохраняемых в Конфигураторе"
+    Я часто работаю с отчетами/обработками, встроенными в конфигурацию, следующим образом.
+    Сохраняя встроенный отчет/обработку в файл, открываю этот файл (Файл - Открыть - поиск файла в папке, как правило, файлов в папке немало, секунду или две теряется  ), исправляю файл и тут же, не перезапуская Конфигуратор, открываю исправленный файл в режиме Предприятия.
+    Думаю, что по такой схеме работаю не я один 
+    И вот у меня родилась мысль немного автоматизировать этот процесс:
+    разработчик сохраняет отчет/обработку в файл (тут как обычно), 
+    а дальше вступает скрипт: перехватывает сохранение файла, узнает имя и путь файла, проверяет расширение этого файла,
+    если это внешний отчет/обработка, то скрипт предлагает немедленно открыть этот файл в Конфигураторе.
+    Планы: В настройках скрипта можно будет указать:
+        всегда открывать такие файлы без вопроса или всегда задавать вопрос,
+        расширения открываемых файлов - по умолчанию erf, epf 
+ */
+var _версияСкрипта = 0.3
+
+var _DefaultExts = "*.epf,*.erf,"; // обязательно указание запятой после расширения !
+
+function Designer::onSelectFile(selectFileData) //As ISelectFileData
+{
+    if(selectFileData.mode != sfSave) return;
+
+	var selDlg = v8New("ДиалогВыбораФайла", РежимДиалогаВыбораФайла.Открытие);
+	selDlg.Заголовок = selectFileData.title;
+	selDlg.ПолноеИмяФайла = selectFileData.initialFileName ;
+
+    Фильтр = "";
+    фильтрПодходит = false;
+    for (var i = 0 ; i < selectFileData.filtersCount; i++) {
+        filterVal = selectFileData.filterVal(i);
+        if (-1 != _DefaultExts.indexOf(filterVal+","))
+            фильтрПодходит = true;
+        Фильтр += selectFileData.filterDescr(i)+" ("+filterVal+")|"+filterVal + "|";
+    }
+    if(!фильтрПодходит) return;
+
+    selDlg.Фильтр += Фильтр;
+
+    selectFileData.result = sfrSelected;
+	if (!selDlg.Выбрать()) return;
+    
+    selectFileData.addSelectedFile(selDlg.ПолноеИмяФайла);
+        //Message(selectFileData.selectedFile(0));
+    OpenFile(selDlg.ПолноеИмяФайла);
+}
+
+var Text = '00000000-0000-0000-0000-000000000000' // простой текст
+var TextOem = '74d75a51-58b7-46b0-931a-f3bac20e596e' // простой текст - кодировка Dos/Oem
+var Epf = '0e0e54cf-253b-4fc9-a895-26897e1a51f7' // обработки
+var Erf = '6d01520c-23c6-4301-86f7-e81268f07ee3' // отчеты
+var Moxel = 'e555a6fe-768f-476a-bf4b-1d945aa56099' // табличный документ
+var Configuration = 'c64ce8a4-a74d-40e9-996e-feadca885e11' // файл конфигурации
+var Template = '03ad782c-900b-4594-bdb7-66ed05992b8b' // файл шаблонов
+
+var kindArray = new Array(Text, TextOem, Epf, Erf, Moxel, Configuration, Template)
+
+function Test()
+{
+    if (!Array.prototype.indexOf) {
+      Array.prototype.indexOf = function (obj, fromIndex) {
+        if (fromIndex == null) {
+            fromIndex = 0;
+        } else if (fromIndex < 0) {
+            fromIndex = Math.max(0, this.length + fromIndex);
+        }
+        for (var i = fromIndex, j = this.length; i < j; i++) {
+            if (this[i] === obj)
+                return i;
+        }
+        return -1;
+      };
+    }
+    assert(0, kindArray.indexOf(Text))
+    assertNot(-1, kindArray.indexOf(Text))
+}
+
+    // try{
+    //    _macrosTestOpenFile()
+    // }
+    // catch(e){
+        // Message("ошибка macrosTestOpenFile() - "+e)
+        // //Message("ошибка macrosTestOpenFile()") // - "+e)
+    // }
+
 // JScript source code
 //24.08.2011 11:34:38 orefkov добавил:
 //Тут возник вопрос по поводу открытия файлов в Конфигураторе.
@@ -24,27 +110,66 @@ global.connectGlobals(SelfScript)
 //Далее просто впихнул свое значение в начало списка, сохранил весь список в профайл, и послал нужную команду.
 //Вот код:
 
-function macrosTestOpenFile()
+function _macrosTestOpenFile()
 {
     //var filepath = "c:\\test\\snegopat\\core\\std\\scripts\\0_global_context.js"
     var filepath = "T:\\Snegopat\\Starter\\scripts\\configCaption.js"
-    OpenFile(filepath)
+
+    Message("1")
+    try{
+        OpenFile(filepath)        
+    }
+    catch(e){ Message("Неудача") }
+
+    Message("2")
+    try{
+        OpenFile(filepath, "")
+    }
+    catch(e){ Message("Неудача") }
+
+    Message("3")
+    try{
+      OpenFile(filepath, "1")
+        Message("Неудача")
+    }
+    catch(e){}
 }
 
-function OpenFile(filepath)
+function assert(p1, p2)
 {
+    if (p1 != p2) 
+        throw "Значение <"+p1+"> не равно <"+p2+">, а ожидали равенство";
+}
+function assertNot(p1, p2)
+{
+    if (p1 == p2) 
+        throw "Значение <"+p1+"> равно <"+p2+">, а ожидали неравенство";
+}
+
+//Еще один параметр в методе OpenFile - тип документа (их лучше задать символьными константами)
+//Если тип не передан, попытаться найти его по расширению файла.
+//Если расширение непонятное, использовать нулевой гуид.
+//
+//то есть чтобы можно было пользоваться так:
+//$addin fileopen of
+//fo.OpenFile(path)
+//fo.OpenFile(path, fo.TextOEM)
+//fo.OpenFile(path, fo.Template)
+//и тд и тп.
+//
+function OpenFile(filepath) //, filekind)
+{
+    Message("Открываем файл "+filepath)
     // Подготовим наше значение для MRU - списка
-    var docKind = '00000000-0000-0000-0000-000000000000' // простой текст
-    var docOemKind = '74d75a51-58b7-46b0-931a-f3bac20e596e' // простой текст - кодировка Dos/Oem
-    var epfKind = '0e0e54cf-253b-4fc9-a895-26897e1a51f7' // обработки
-    var erfKind = '6d01520c-23c6-4301-86f7-e81268f07ee3' // отчеты
-    var mxlKind = 'e555a6fe-768f-476a-bf4b-1d945aa56099' // табличный документ
-    var mxlKind = 'c64ce8a4-a74d-40e9-996e-feadca885e11' // файл конфигурации
-    var stKind = '03ad782c-900b-4594-bdb7-66ed05992b8b' // файл шаблонов
+    kind = Text
+        // if (filekind)
+            // kind = filekind
+        // if (-1 == kindArray.indexOf(kind))
+            // throw "Неверный параметр filekind = "+filekind
     
     var mruItem = ЗначениеИзСтрокиВнутр('{"#",36973550-6bbb-11d5-bf72-0050bae2bc79,\n' +
     '{1,\n' +
-    '{"file://' + filepath + '",0},' + docKind + '}\n' +
+    '{"file://' + filepath + '",0},' + kind + '}\n' +
     '}')
 
     // Получим текущий список MRU из настроек
@@ -61,22 +186,21 @@ function OpenFile(filepath)
     // Сохраним MRU-список обратно в настройки
     profileRoot.setValue("App/MRUFileList", mru)
     // И зашлем команду
-    //var cmd = addins.byUniqueName("stdcommands").object
-    //cmd.Frame.RecentFile.getState()
-    //cmd.Frame.RecentFile.send(0)
     stdcommands.Frame.RecentFile.getState()
     stdcommands.Frame.RecentFile.send(0)
+    
+    return true
 }
 
 //Вот так пытливый ум преодолевает преграды.
 
 //ЗЫ: Для просмотра MRU использовался следующий код:
-function ИсследоватьMRU()
+function macrosИсследоватьMRU()
 {
     var mru = profileRoot.getValue("App/MRUFileList")
     for(var i = new Enumerator(mru); !i.atEnd(); i.moveNext())
     {
-	    Message("v=" + ЗначениеВСтрокуВнутр(i.item().Значение), mExc3)
+        Message("v=" + ЗначениеВСтрокуВнутр(i.item().Значение), mExc3)
     }
 }
 //В просмотре профайла мне помогло scripts\devtools\pflview.js
