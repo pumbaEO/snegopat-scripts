4173ebde0394d14970e5eccd3464e700213600a0
kuntashov <kuntashov>
2011-11-12 22:29:56 +0000

    Добавлен тест-скрипт для метода stdlib.require()

---
+++ Tests/Automated/stdlib_require/lib.js
@@ -0,0 +1,17 @@
+﻿$engine JScript
+$uname testStdlibRequire_Lib
+$dname Скрипт-библиотека для тестирования метода stdlib::require()
+
+var publicObject = {
+    'myProperty' : "Hello, I am public object!"
+}
+
+function publicMethod()
+{
+    return "Hello, I'm public method!";
+}
+
+function _privateMethod()
+{
+    return "Hello, I'm private method!";
+}
