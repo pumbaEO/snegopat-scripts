aad1a55961ad7311b9d17d0743da11fe7e190216
vsuh <vsuh>
2012-03-30 08:35:52 +0000

    Реализована перезапись предыдущего описания

--- ModuleComment.js
+++ ModuleComment.js
@@ -14,15 +14,25 @@ global.connectGlobals(SelfScript);
 ////
 //// Автор: Сухих В.Ю. по мотивам скрипта author.js Александра Кунташова
 ////                                      <kuntashov@gmail.com>, http://compaud.ru/blog
+//// 29.03.2012 Список отображаемых в форме конфигураций настраивается в Списке значений дляКонфигурации
+//// 			 для включения возможности удалите комментарии "//ft"
 ////}
 ////////////////////////////////////////////////////////////////////////////////////////
 var doOK = false;
+//ft var дляКонфигурации = v8New("СписокЗначений");
+//ft	дляКонфигурации.Add(	 		"АСБНУ 1.3"				);
+//ft	дляКонфигурации.Add( 			"ЗУП 2.5.48"			);
+//ft	дляКонфигурации.Add( 			"УПП 1.2 (рзп)"			);
+//ft	дляКонфигурации.Add( 			"УАТ"					);
+//ft	дляКонфигурации.Add( 			"Для всех конфигураций"	);
+
 ////////////////////////////////////////////////////////////////////////////////////////
 ////{ Макросы
 ////
 
 SelfScript.self['macrosМаркер "Вставить"'] = function() {
     form = loadScriptForm(SelfScript.fullPath.replace(/js$/, 'ssf'), SelfScript.self);
+//ft	form.ЭлементыФормы.Назначение.СписокВыбора = дляКонфигурации;
     form.DoModal();
  	
    form = null;
@@ -68,18 +78,26 @@ function addMarker(markerType) {
     var w = GetTextWindow();
     if (!w) return;
 
+//debugger;	
+	var intLastStrCommentAlready = CheckForDescription(w);
+
+	if (intLastStrCommentAlready != 1) 
+	{
+		w.textWindow.SetSelection(1, 1, intLastStrCommentAlready, 90);
+		w.textWindow.SelectedText = "";
+	}
 	var synonym = Метаданные.Синоним;
  	w.SetCaretPos(1, 1);
 	w.InsertLine(1, ""+
-			"//*******************************************\n"+
+			"//****************************************//_\\\\\n"+
 			"// Описание программы от " +
 			dtNow +"  /{"+
-			"\n// ___" +					Settings['ТипПрограммы'] +
-			"___\n// предназначено для [" +	Settings['Назначение'] +
-			"]\n" + 						Settings['Описание'].replace(/(^.*)/mg, "//>>  $1") +
-			"\n//                " + 		Settings['Автор'] +
+			"\n// ___" +					strct1C_Description.ТипПрограммы +
+			"___\n// предназначено для [" +	strct1C_Description.Назначение +
+			"]\n" + 						strct1C_Description.Описание.replace(/(^.*)/mg, "//>>  $1") +
+			"\n//                " + 		strct1C_Description.Автор +
 			"\n// Создан в " + 				synonym +
-			"\n"    +	"//***************************************//}\n");
+			"\n"    +	"//***************************************//}}\n");
 
 }
 function parseTpl() {
@@ -89,10 +107,10 @@ function parseTpl() {
     return snegopat.parseTemplateString('<?"", ' + a.join(',') + '>');
 }
 
-function getSettings() {
+function getSettingsStructure() {
 
     var s = v8New("Структура");
-  
+	
     s.Вставить("Автор", "");
     s.Вставить("Назначение", "");
     s.Вставить("ТипПрограммы", "");
@@ -108,13 +126,13 @@ function getSettings() {
 
 //{ Обработчики элементов управления формы
 function ПриОткрытии () {
- 	ЗаполнитьЗначенияСвойств(form, Settings);
+ 	ЗаполнитьЗначенияСвойств(form, strct1C_Description);
 	form['Описание'] = "";
 }
 
 function КнопкаОкНажатие (Элемент) {
-    ЗаполнитьЗначенияСвойств(Settings, form);
-    profileRoot.setValue(pflSuhAuthorJs, Settings);
+    ЗаполнитьЗначенияСвойств(strct1C_Description, form);
+    profileRoot.setValue(pflSuhAuthorJs, strct1C_Description);
 	doOK = true;
     form.Close();
 }
@@ -126,14 +144,30 @@ function КнопкаОтменаНажатие (Элемент) {
 
 //} Обработчики элементов управления формы
 
+//{ Вернем последнюю строку описания модуля, если есть иначе 0
+function CheckForDescription(wnd) {
+	var txtText = wnd.ПолучитьТекст();
+	if(wnd.LinesCount == 0) return 0;
+	var arrLines = wnd.GetLines();
+	if (arrLines[0].search(/\*\/\/\_\\\\/) == -1) return 0;
+	
+	for (var ln in arrLines)
+	{
+		if (arrLines[ln].search(/\/\/\}\}/) > 0) return (parseInt(ln) + 1);
+		if (arrLines[ln].search(/^\s*\/\//) == -1) return 0;
+	}
+	return 0;
+}
+//} CheckForDescription
+
 //{ Горячие клавиши по умолчанию.
 function getPredefinedHotkeys(predef) {
-    predef.setVersion(1.6);
+    predef.setVersion(2.2);
     predef.add('Маркер "Вставить"', "Alt + Z");
 }
 //} Горячие клавиши по умолчанию.
 
-var Settings = getSettings();
+var strct1C_Description = getSettingsStructure();
 
 
 
