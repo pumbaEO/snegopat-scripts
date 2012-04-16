0c4ad63ac46da33bd8d6346dc9aa5ab98fe9083b
orefkov <orefkov>
2012-04-16 13:20:26 +0000

    начата библиотека работы с WinAPI через dynwrapx.dll

---
+++ Libs/winapi.js
@@ -0,0 +1,69 @@
+﻿$engine JScript
+$uname winapi
+$dname Библиотека доступа к WinAPI посредством dynwrapx
+$addin stdlib
+
+var api;
+
+(function()
+{
+    try{
+    api = new ActiveXObject("DynamicWrapperX")
+    }catch(e)
+    {
+        Message("winapi.js: не удалось создать DynamicWrapperX. " + e.description)
+        return
+    }
+    regs = [
+    {
+        lib: "USER32.DLL",
+        funcs:[
+            ["GetWindowRect", "i=lp", "r=l"]
+        ]
+    }
+    ]
+    for(var k in regs)
+    {
+        for(var i in regs[k].funcs)
+            api.Register(regs[k].lib, regs[k].funcs[i][0], regs[k].funcs[i][1], regs[k].funcs[i][2])
+    }
+    
+})();
+
+Rect = stdlib.Class.extend(
+{
+    construct: function(l, t, r, b)
+    {
+        this.left = l
+        this.top = t
+        this.right = r
+        this.bottom = b
+    },
+    width: function()
+    {
+        return this.right - this.left
+    },
+    height: function()
+    {
+        return this.bottom - this.top
+    }
+})
+
+RectApi = stdlib.Class.extend(
+{
+    construct: function()
+    {
+        this.mem = api.Space(16)
+    },
+    toRect: function()
+    {
+        return new Rect(api.NumGet(this.mem, 0, "l"), api.NumGet(this.mem, 4, "l"), api.NumGet(this.mem, 8, "l"), api.NumGet(this.mem, 12, "l"))
+    }
+})
+
+function GetWindowRect(hwnd)
+{
+    var rect = new RectApi()
+    api.GetWindowRect(hwnd, rect.mem)
+    return rect.toRect()
+}
--- choiceHelpTopic.js
+++ choiceHelpTopic.js
@@ -1,18 +1,16 @@
 ﻿$engine JScript
 $uname choiceHelpTopic
 $dname Выбрать главу справки
+$addin stdlib
 
 // (c) Александр Орефков
 // Скрипт позволяет быстрее выбрать нужную главу справки, когда одному слову
 // соответствует несколько разделов
 
 try{
-
-var wrap = new ActiveXObject("DynamicWrapperX")
-wrap.Register("USER32.DLL", "GetWindowRect", "i=lp", "r=l")
+var api = stdlib.require("winapi.js")
 var sel = new ActiveXObject('Svcsvc.Service')
 events.connect(windows, "onDoModal", SelfScript.self)
-
 }catch(e)
 {
     Message("choiceHelpTopic.js: ошибка при инициализации. " + e.description)
@@ -32,24 +30,8 @@ function onDoModal(dlgInfo)
                 for(var rows = new Enumerator(vt); !rows.atEnd(); rows.moveNext())
                     choices.push(rows.item().Get(0))
                 //debugger
-                if(wrap)
-                {
-                    var Rect = wrap.Space(16)
-                    var pStr = wrap.StrPtr(Rect)
-                    wrap.GetWindowRect(dlgInfo.form.getControl(-1).hwnd, Rect)
-                    var left = wrap.NumGet(pStr, 0, "l")
-                    var top = wrap.NumGet(pStr, 4, "l")
-                    var right = wrap.NumGet(pStr, 8, "l")
-                    var bottom = wrap.NumGet(pStr, 12, "l")
-                }
-                else
-                {
-                    var left = 100
-                    var top = 300;
-                    var right = 900;
-                    var bottom = 700;
-                }
-                var choice = sel.FilterValue(choices.join("\r\n"), 1 | 8 | 32, 'Выберите главу', left - 10, top - 20, right - left + 20, bottom - top + 40)
+                var rect = api.GetWindowRect(dlgInfo.form.getControl(-1).hwnd)
+                var choice = sel.FilterValue(choices.join("\r\n"), 1 | 8 | 32, 'Выберите главу', rect.left - 10, rect.top - 20, rect.width() + 20, rect.height() + 40)
                 if(choice.length)
                 {
                     var sk = ""
