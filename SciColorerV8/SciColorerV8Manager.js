6412ff105e9e95784810886953908b55d01ca2b7
metaeditor <metaeditor>
2012-06-15 10:53:17 +0000

    \SciColorerV8\SciColorerV8.dll

---
+++ SciColorerV8/SciColorerV8Manager.js
@@ -0,0 +1,13 @@
+﻿$engine JScript
+$uname SciColorerV8Manager
+$dname SciColorerV8 Manager
+$addin stdcommands
+
+function macrosOnHyperLinkClick(){ //вызывается при Ctrl+Click на любом идентификаторе в тексте модуля
+	stdcommands.Frntend.GoToDefinition.getState()
+	stdcommands.Frntend.GoToDefinition.send()
+}
+
+function macrosOnLineNumbersContextMenu(){ //вызывается при правом клике на номерах строк
+	addins.byUniqueName("SciColorerV8").invokeMacros("Развернуть все"); //например
+}
