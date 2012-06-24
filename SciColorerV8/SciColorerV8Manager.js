7c9bac23a7f7ae48f57d1cd68dfaeb8f8898e9b1
metaeditor <metaeditor>
2012-06-24 12:29:38 +0000

    SciColorerV8\SciColorerV8.dll

--- SciColorerV8/SciColorerV8Manager.js
+++ SciColorerV8/SciColorerV8Manager.js
@@ -26,10 +26,22 @@ function macrosРазвернутьВсе()
     addins.byUniqueName("SciColorerV8").invokeMacros("Развернуть все")
 }
 
+function macrosПрокруткаСтрокиВверх()
+{
+    addins.byUniqueName("SciColorerV8").invokeMacros("Прокрутка строки вверх")
+}
+
+function macrosПрокруткаСтрокиВниз()
+{
+    addins.byUniqueName("SciColorerV8").invokeMacros("Прокрутка строки вниз")
+}
+
 function getPredefinedHotkeys(predef)
 {
-    predef.setVersion(1)
+    predef.setVersion(2)
     predef.add("СвернутьРазвернутьГруппу", "Ctrl + NumAdd")
     predef.add("РазвернутьВсе", "Ctrl + Shift + NumAdd")
     predef.add("СвернутьВсе", "Ctrl + Shift + Num-")
+    predef.add("ПрокруткаСтрокиВверх", "Ctrl + Up")
+    predef.add("ПрокруткаСтрокиВниз", "Ctrl + Down")
 }
