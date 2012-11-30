7f2a96198b72a474af8153acc55d42e0b760fcc8
kuntashov <kuntashov>
2012-11-30 23:00:01 +0000

    Добавлен пример использования метода stdlib.setTimeout()

---
+++ Tests/Examples/stdlib_setTimeout_Example.js
@@ -0,0 +1,13 @@
+﻿$engine JScript
+$uname Example_stdlib_setTimeout
+$dname Пример использования setTimeout
+$addin stdlib
+
+SelfScript.self['macrosВывести сообщение с задержкой 5 секунд'] = function() {
+	
+	// В качестве первого аргумента передаем анонимную функцию,
+	// которая будет выводить сообщение. Вторым - время задержки (в миллисекундах).
+	
+	stdlib.setTimeout(function() { Message('Прошло 5 секунд!'); }, 5000);
+	
+}
