663259e3f7d10e5a15961be82d69df8f06879fc2
kuntashov <kuntashov>
2013-01-21 04:19:39 +0000

    Добавлен пример использования библиотеки log4js.

---
+++ Tests/Examples/loggerExample.js
@@ -0,0 +1,30 @@
+﻿$engine JScript
+$uname testLog
+$dname Тест log
+$addin stdlib
+$addin hotkeys
+
+stdlib.require("log4js.js", SelfScript);
+
+function macrosTest1() {
+	
+	var log4jsLogger = Log4js.getLogger("Log4js");
+	
+	log4jsLogger.addAppender(new Log4js.BrowserConsoleAppender());
+	log4jsLogger.setLevel(Log4js.Level.ALL);
+	
+	log4jsLogger.debug("test");
+}
+
+function macrosTest2() {
+
+	var log4jsLogger = Log4js.getLogger("Log4js");
+	
+	var appender = new Log4js.BrowserConsoleAppender();
+	appender.setLayout(new
+	Log4js.PatternLayout(Log4js.PatternLayout.TTCC_CONVERSION_PATTERN));
+	log4jsLogger.addAppender(appender);
+	log4jsLogger.setLevel(Log4js.Level.DEBUG);
+
+	log4jsLogger.debug("test");
+}
